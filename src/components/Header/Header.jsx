06e409ab826328db139709ce5a28d1b1e7a8ec25
Juwel-771 <juwelah99@gmail.com>
2025-04-29 01:58:17 +0600

    show task created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,6 +9,11 @@ const Header = () => {
             </div>
             <div className="themeSelector">
                 <span className="light"></span>
+                <span className="medium"></span>
+                <span className="dark"></span>
+                <span className="gOne"></span>
+                <span className="gTwo"></span>
+                <span className="gThree"></span>
             </div>
         </header>
     );
--- src/components/ShowTask/ShowTask.jsx
+++ src/components/ShowTask/ShowTask.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+const task = [
+    {id: 1001, name: "Task A", time: "2:09:01 AM 9/14/2024"},
+    {id: 1002, name: "Task B", time: "2:10:01 AM 9/14/2024"},
+    {id: 1003, name: "Task C", time: "2:11:01 AM 9/14/2024"},
+
+]
+
 
 const ShowTask = () => {
     return (
-        <div>
-            <p>Show Task</p>
-        </div>
+       <section className='showTask'>
+            <div className="head">
+                <div>
+                    <span className='title' >Task Lis</span>
+                    <span className='count'>0</span>
+                </div>
+                <button className='clearAll'>Clear All</button>
+            </div>
+            <ul>
+                {
+                    task.map( tasks => 
+                    <li>
+                        <p>
+                            <span className="name">{tasks.name}</span>
+                            <span className="time">{tasks.time}</span>
+                        </p>
+                        <i className='bi bi-pencil-square'></i>
+                        <i className='bi bi-trash'></i>
+                    </li>
+                    )
+                }
+            </ul>
+       </section>
     );
 };
 
